cf76a2e30cbdde179ee188471e312857df52e62b
1a11a <86703816+1a11a@users.noreply.github.com>
2023-10-28 20:35:13 +0530

    register

--- login-form/script.js
+++ login-form/script.js
@@ -6,3 +6,29 @@ labels.forEach(label => {
         .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
         .join('')
 })
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById("login-form");
+    const registerForm = document.getElementById("register-form");
+    const registerLink = document.getElementById("register-link");
+    const loginLink = document.getElementById("login-link");
+
+    registerLink.addEventListener("click", function (event) {
+        event.preventDefault(); // Prevent the link from navigating
+
+        // Toggle the display of the login and registration forms
+        loginForm.style.display = "none";
+        registerForm.style.display = "block";
+    });
+
+    loginLink.addEventListener("click", function (event) {
+        event.preventDefault(); // Prevent the link from navigating
+
+        // Toggle the display of the login and registration forms
+        loginForm.style.display = "block";
+        registerForm.style.display = "none";
+    });
+});
+
+
